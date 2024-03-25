c5be6c8293d8ae2afc7e4d79ea086f5f13d109eb
Pathum Bandara <pathumbandarame@gmail.com>
2024-03-25 19:01:45 +0530

    changes

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&amp;display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&amp;display=swap" rel="stylesheet">
+
+    {{-- styles --}}
+    <link rel="stylesheet" href="{{ asset('css/animate.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/aos.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap-select.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/custom-animate.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/fancybox.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/flaticon.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/imp.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/jquery-ui.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/magnific-popup.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/owl.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/rtl.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/scrollbar.css') }}">
+
+    <!-- Module CSS -->
+    <link rel="stylesheet" href="{{ asset('css/header-section.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/banner-section.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/about-section.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/blog-section.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/fact-counter-section.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/faq-section.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/contact-page.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/breadcrumb-section.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/team-section.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/partner-section.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/testimonial-section.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/services-section.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/footer-section.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
+
+    {{-- <meta name="csrf-token" content="{{ csrf_token() }}"> --}}
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="@yield('description')">
+    <meta name="keywords" content="@yield('keywords')">
+    <meta name="author" content="PhyxleLoom">
+</head>
+<body>
+
+    <script src="{{ asset('js/jquery.js') }}"></script>
+    <script src="{{ asset('js/aos.js') }}"></script>
+    <script src="{{ asset('js/appear.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap-select.min.js') }}"></script>
+    <script src="{{ asset('js/isotope.js') }}"></script>
+    <script src="{{ asset('js/jquery.countTo.js') }}"></script>
+    <script src="{{ asset('js/jquery.easing.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.enllax.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.fancybox.js') }}"></script>
+    <script src="{{ asset('js/jquery.magnific-popup.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.paroller.min.js') }}"></script>
+    <script src="{{ asset('js/jquery-ui.js') }}"></script>
+    <script src="{{ asset('js/knob.js') }}"></script>
+    <script src="{{ asset('js/map-script.js') }}"></script>
+    <script src="{{ asset('js/owl.js') }}"></script>
+    <script src="{{ asset('js/pagenav.js') }}"></script>
+    <script src="{{ asset('js/parallax.min.js') }}"></script>
+    <script src="{{ asset('js/scrollbar.js') }}"></script>
+    <script src="{{ asset('js/TweenMax.min.js') }}"></script>
+    <script src="{{ asset('js/validation.js') }}"></script>
+    <script src="{{ asset('js/wow.js') }}"></script>
+    <script src="{{ asset('js/custom.js') }}"></script>
+</body>
